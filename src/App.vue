0e57d396f06132b1102606804551dcd36e94349c
Carter Snook <cartersnook04@gmail.com>
2020-09-28 07:12:33 -0500

    Added await to async function call

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,7 @@ export default {
 
     async function updateJokesForm(e) {
       e.preventDefault();
-      updateJokes();
+      await updateJokes();
     }
 
     return { jokes, jokeQuery, updateJokesForm };
